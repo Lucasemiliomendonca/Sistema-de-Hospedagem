a14cbe37a75107e801af28e777b16a543f2a6aa3
Lucas Emilio Mendonça <mendoncae.metrocasa@gmail.com>
2025-07-20 19:45:53 -0300

    Reserva.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,8 +15,6 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
             if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
@@ -24,8 +22,6 @@ namespace DesafioProjetoHospedagem.Models
             else
             {
                 throw new Exception("O número de hóspedes é maior que a capacidade da suíte.");
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
             }
         }
 
@@ -36,24 +32,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             int quantidade = Hospedes.Count;
             return quantidade;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor *= 0.9M;
+
+
+
+                
             }
 
             return valor;
